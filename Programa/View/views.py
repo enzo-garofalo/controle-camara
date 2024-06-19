5504544f658f070246817e314b91bfeae2378cfd
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-06-19 17:55:24 -0300

    Feat: Buttons

--- Programa/View/views.py
+++ Programa/View/views.py
@@ -1,11 +1,21 @@
 from flask import render_template
 from main import app
 
+class Produto():
+    def __init__(self, nome, quantidade, tipo, data):
+        self.nome = nome
+        self.quantidade = quantidade
+        self.tipo = tipo
+        self.data = data
+
+produto1 = Produto('Leitinho Trufado', 2, 'Pote 1 Litro', '10/10/24')
+produto2 = Produto('Chocolate c/ avelã', 4, 'Pote 1 Litro', '10/10/24')
+produto3 = Produto('Leitinho Trufado', 6, 'Pote 2 Litros', '10/10/24')
+produto4 = Produto('Morango C/ Chocolate', 2, 'Pote 2 Litros', '10/10/24')
+produto5 = Produto('Creme', 2, 'Pote 5 Litro', '10/10/24')
+sorvetes = [produto1, produto2, produto3, produto4, produto5]
 
 @app.route('/')
 def index():
-    sorvetes= {'1 litro':['Leitinho Trufado', 3, '12/05/24'], 
-               '2 litros':['Leitinho Trufado', 4, '12/12/23'],
-               '5 litros':['Chocolate c/ Pedaços', 5, '10/12/23']}
 
-    return render_template('index.html', titulo='Controle De Estoque', sorvetes = sorvetes)
+    return render_template('index.html', titulo='Produtos na Camara', sorvetes = sorvetes)
