b67cdd752a7d3871936156a29d2abe64d9feb58b
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-06-20 13:39:25 -0300

    Feat: List product now is grouped by type

--- Programa/View/views.py
+++ Programa/View/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect
 from main import app
+from collections import defaultdict
 
 class Produto():
     def __init__(self, nome, quantidade, tipo, data):
@@ -13,9 +14,19 @@ produto2 = Produto('Chocolate c/ avelã', 4, 'Pote 1 Litro', '10/10/24')
 produto3 = Produto('Leitinho Trufado', 6, 'Pote 2 Litros', '10/10/24')
 produto4 = Produto('Morango C/ Chocolate', 2, 'Pote 2 Litros', '10/10/24')
 produto5 = Produto('Creme', 2, 'Pote 5 Litro', '10/10/24')
-sorvetes = [produto1, produto2, produto3, produto4, produto5]
+produto6 = Produto('Três Chocolates', 2, 'Pote 5 Litro', '10/10/24')
+sorvetes = [produto1, produto2, produto3, produto4, produto5, produto6]
 
+lista = defaultdict(list)
+for sorvete in sorvetes:
+    lista[sorvete.tipo].append([sorvete.nome, sorvete.quantidade, sorvete.data])
+
+lista = dict(lista)
 @app.route('/')
 def index():
 
-    return render_template('index.html', titulo='Produtos na Camara', sorvetes = sorvetes)
+    return render_template('index.html', titulo='Produtos na Câmara', sorvetes = sorvetes, lista=lista)
+
+@app.route('/chegada')
+def chegada():
+    return render_template('chegada.html', titulo='Selecione qual Produto Chegou')
