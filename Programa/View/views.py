83cad9cf6000f2a2e5e21df23fdf2f167227cae9
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-06-21 20:02:38 -0300

    Feat: buttons now works

--- Programa/View/views.py
+++ Programa/View/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request, session
 from main import app
 from collections import defaultdict
 
@@ -11,11 +11,13 @@ class Produto():
 
 produto1 = Produto('Leitinho Trufado', 2, 'Pote 1 Litro', '10/10/24')
 produto2 = Produto('Chocolate c/ avelã', 4, 'Pote 1 Litro', '10/10/24')
+produto7 = Produto('Leitinho c/ Frutas Vermelhas', 4, 'Pote 1 Litro', '10/10/24')
+produto7 = Produto('Mousseee', 4, 'Pote 1 Litro', '10/10/24')
 produto3 = Produto('Leitinho Trufado', 6, 'Pote 2 Litros', '10/10/24')
 produto4 = Produto('Morango C/ Chocolate', 2, 'Pote 2 Litros', '10/10/24')
 produto5 = Produto('Creme', 2, 'Pote 5 Litro', '10/10/24')
 produto6 = Produto('Três Chocolates', 2, 'Pote 5 Litro', '10/10/24')
-sorvetes = [produto1, produto2, produto3, produto4, produto5, produto6]
+sorvetes = [produto1, produto2, produto3, produto4, produto5, produto6, produto7]
 
 lista = defaultdict(list)
 for sorvete in sorvetes:
@@ -27,6 +29,17 @@ def index():
 
     return render_template('index.html', titulo='Produtos na Câmara', sorvetes = sorvetes, lista=lista)
 
-@app.route('/chegada')
-def chegada():
-    return render_template('chegada.html', titulo='Selecione qual Produto Chegou')
+@app.route('/redireciona')
+def redireciona():
+    funcao = request.args.get('funcao')
+    session['funcao'] = funcao
+    return render_template('redireciona.html', titulo=f'Selecione o Produto de {funcao}')
+
+@app.route('/funcionalidade', methods=['POST', ])
+def funcionalidade():
+    value = request.form['valor']
+    funcao = session.get('funcao')
+    produtos = lista.get(value)
+    nova_lista = {}
+    nova_lista[value] = produtos
+    return render_template('index.html', titulo=value+funcao, lista=nova_lista)
