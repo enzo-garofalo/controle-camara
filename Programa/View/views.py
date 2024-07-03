2dfc460562b19b6725ba9c70acfb432607492b49
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-03 08:41:01 -0300

    Reestruturando código

--- Programa/View/views.py
+++ Programa/View/views.py
@@ -1,29 +1,8 @@
 from flask import render_template, redirect, request, session
 from main import app
 from collections import defaultdict
+from View.models import sorvetes, lista
 
-class Produto():
-    def __init__(self, nome, quantidade, tipo, data):
-        self.nome = nome
-        self.quantidade = quantidade
-        self.tipo = tipo
-        self.data = data
-
-produto1 = Produto('Leitinho Trufado', 2, 'Pote 1 Litro', '10/10/24')
-produto2 = Produto('Chocolate c/ avelã', 0, 'Pote 1 Litro', '10/10/24')
-produto7 = Produto('Leitinho c/ Frutas Vermelhas', 4, 'Pote 1 Litro', '10/10/24')
-produto7 = Produto('Mousseee', 4, 'Pote 1 Litro', '10/10/24')
-produto3 = Produto('Leitinho Trufado', 6, 'Pote 2 Litros', '10/10/24')
-produto4 = Produto('Morango C/ Chocolate', 2, 'Pote 2 Litros', '10/10/24')
-produto5 = Produto('Creme', 2, 'Pote 5 Litro', '10/10/24')
-produto6 = Produto('Três Chocolates', 2, 'Pote 5 Litro', '10/10/24')
-sorvetes = [produto1, produto2, produto3, produto4, produto5, produto6, produto7]
-
-lista = defaultdict(list)
-for sorvete in sorvetes:
-    lista[sorvete.tipo].append([sorvete.nome, sorvete.quantidade, sorvete.data])
-
-lista = dict(lista)
 @app.route('/')
 def index():
 
@@ -42,4 +21,4 @@ def funcionalidade():
     produtos = lista.get(value)
     nova_lista = {}
     nova_lista[value] = produtos
-    return render_template('funcionalidade.html', titulo=value+funcao, lista=nova_lista, funcao=funcao)
+    return render_template('funcionalidade.html', titulo=value, lista=nova_lista, funcao=funcao)
