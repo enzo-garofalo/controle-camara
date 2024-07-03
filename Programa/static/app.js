8961ac3cb69009c3c0d5288c86f8424ea29d3f10
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-03 15:51:21 -0300

    Implementando JS

--- Programa/static/app.js
+++ Programa/static/app.js
@@ -1,8 +1,34 @@
-var aumentar = document.getElementById('aumentar')
-var diminuir = document.getElementById('diminuir')
-var control = 0
+document.addEventListener('DOMContentLoaded', function() {
+  var funcao = document.querySelector('.content').getAttribute('data-funcao');
 
-aumentar = document.addEventListener('click', (e)=>{
-  control++
-  var text= document.getElementById('qtd').innerHTML = control
-})
+  document.querySelector('.content').addEventListener('click', function(event) {
+      if (event.target.classList.contains('adicionar') || event.target.classList.contains('remover')) {
+          var index = event.target.getAttribute('data-index');
+          var qtdElement = document.getElementById('qtd-' + index);
+          var currentQtd = parseInt(qtdElement.innerHTML);
+
+          if (funcao === 'Retirada') {
+              var retirarElement = document.getElementById('retirar-' + index);
+              var currentRetirar = parseInt(retirarElement.innerHTML);
+
+              if (event.target.classList.contains('adicionar')) {
+                  if (currentRetirar < currentQtd) {
+                      retirarElement.innerHTML = currentRetirar + 1;
+                  }
+              } else if (event.target.classList.contains('remover')) {
+                  if (currentRetirar > 0) {
+                      retirarElement.innerHTML = currentRetirar - 1;
+                  }
+              }
+          } else {
+              if (event.target.classList.contains('adicionar')) {
+                  qtdElement.innerHTML = currentQtd + 1;
+              } else if (event.target.classList.contains('remover')) {
+                  if (currentQtd > 0) {
+                      qtdElement.innerHTML = currentQtd - 1;
+                  }
+              }
+          }
+      }
+  });
+});
